75f18d01c17a92c9ca005954d44c895fb404c13c
Oleg S <leshiyoleg@mail.ru>
2022-04-23 17:56:04 +0300

    Added random digits sum in Example 003

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,12 @@
-﻿// Нахождение суммы двух чисел
-int numberA = 31;
-int numberB = 51;
-int result = numberA + numberB;
-Console.WriteLine(result);
+﻿// Нахождение суммы двух чисел - чтобы не удалять первые строки - сделал из них коммент
+//int numberA = 31;
+//int numberB = 51;
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10); //выбирается рандомное число от 1 (включ) до 10 (не вкючит)
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int resultSum = numberA + numberB;
+Console.WriteLine(resultSum);
