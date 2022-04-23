f82e9f65beaceaeef792b8208f04ddff03fcb936
Oleg S <leshiyoleg@mail.ru>
2022-04-23 18:09:26 +0300

    Example 005 If Else User hello

---
+++ Example005_IfElse_hello_user/Program.cs
@@ -0,0 +1,13 @@
+﻿//Особое приветствие любимчика
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "олег")
+{
+    Console.WriteLine("Ура, это ж ОЛЕДЖИО!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
