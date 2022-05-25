93c0e6fe7ec4384277eb0666bd87c461988d4d6b
Oleg S <leshiyoleg@mail.ru>
2022-05-25 10:19:05 +0300

    Added examples from the 4th Lection: Matrix_printing(in Recursion_project), Picture_drawing, Factorial, Fibonacci

---
+++ Example013_2_Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа для вычисления факториала методом рекурсии
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n* Factorial(n-1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
---
+++ Example013_Recursion/Program.cs
@@ -0,0 +1,43 @@
+﻿// Создаем двумерный массив
+// string[,] table = new string[2,5];
+// table[0,0]   table[0,1]  table[0,2]  table[0,3]
+// table[1,0]   table[1,1]  table[1,2]  table[1,3]
+
+// table[1,2]="слово";
+
+// for (int rows = 0; rows<2; rows++)
+// {
+//     for (int columns=0;columns<5;columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
